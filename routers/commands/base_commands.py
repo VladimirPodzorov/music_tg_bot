3964b0f30fa528ac8e8b243467131aec70e186f4
vladimir <podzorof@mail.ru>
2024-07-31 19:24:47 +0300

    create command help

--- routers/commands/base_commands.py
+++ routers/commands/base_commands.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 
@@ -12,3 +12,8 @@ async def command_start_handler(message: Message) -> None:
     This handler receives messages with `/start` command
     """
     await message.answer(f"Hello, {message.from_user.full_name}")
+
+
+@router.message(Command("help"))
+async def command_help_handler(message: Message):
+    await message.answer("Введите в поиск название группы или название песни.")
