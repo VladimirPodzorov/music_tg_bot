611cab5496d948851df12ad1e285253499e443b3
vladimir <podzorof@mail.ru>
2024-08-01 20:08:42 +0300

    add router.message: album, year

--- routers/commands/admin_commands.py
+++ routers/commands/admin_commands.py
@@ -23,4 +23,14 @@ async def add_band(message: Message, state: FSMContext):
 
 
 @router.message(Form.album)
-async def add_album(message: Message, state: FSMContext): ...
+async def add_album(message: Message, state: FSMContext):
+    await state.update_data(album=message.text)
+    await state.set_state(Form.year)
+    await message.answer("Год выхода альбома?")
+
+
+@router.message(Form.year)
+async def add_year(message: Message, state: FSMContext):
+    await state.update_data(year=message.text)
+    await state.set_state(Form.song)
+    await message.answer("Название песни?")
