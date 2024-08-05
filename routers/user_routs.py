326a039e254f5779803701ba8f7270bf1fed7299
vladimir <podzorof@mail.ru>
2024-08-05 21:36:26 +0300

    corrected search_song

--- routers/user_routs.py
+++ routers/user_routs.py
@@ -1,7 +1,7 @@
 from aiogram import Router
 from aiogram.types import Message
 
-from database.crud import get_artist
+from database.crud import get_albums_or_song
 from keyboards.inline_keyboards import build_artist_kb
 
 router = Router(name=__name__)
@@ -10,5 +10,8 @@ router = Router(name=__name__)
 @router.message()
 async def search_song(message: Message):
     artist = message.text.title()
-    result = get_artist(artist)
-    await message.answer(text="Альбомы", reply_markup=build_artist_kb(result))
+    result = get_albums_or_song(artist)
+    if isinstance(result, list):
+        await message.answer(text="Альбомы", reply_markup=build_artist_kb(result))
+    else:
+        await message.answer("backend пока не готов")
