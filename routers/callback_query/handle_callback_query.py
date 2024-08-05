90217bd4f565b2422b1e72eaf72354b72b88e256
vladimir <podzorof@mail.ru>
2024-08-05 22:54:17 +0300

    add handler handle_album

--- routers/callback_query/handle_callback_query.py
+++ routers/callback_query/handle_callback_query.py
@@ -1,3 +1,14 @@
 from aiogram import Router
+from aiogram.types import CallbackQuery
+
+from database.crud import get_playlist
 
 router = Router(name=__name__)
+
+
+@router.callback_query()
+async def handle_album(callback_query: CallbackQuery):
+    await callback_query.answer()
+    album = get_playlist(callback_query.data)
+    for audio_id in album:
+        await callback_query.message.answer_audio(audio=audio_id)
