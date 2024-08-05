6827afe73938d04bc3c8c2c09cef5537ffc949f0
vladimir <podzorof@mail.ru>
2024-08-05 22:51:52 +0300

    fix callback_data=f"{name_button}"

--- keyboards/inline_keyboards.py
+++ keyboards/inline_keyboards.py
@@ -6,6 +6,6 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def build_artist_kb(lst_name_button: List):
     builder = InlineKeyboardBuilder()
     for name_button in lst_name_button:
-        builder.button(text=f"{name_button}", callback_data="artist")
+        builder.button(text=f"{name_button}", callback_data=f"{name_button}")
     builder.adjust(1)
     return builder.as_markup()
